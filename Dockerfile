bb94c60c4a9df3280359c114a759f1db4e15ceb6
Century-ss <hgfc7543ggfdch@gmail.com>
2024-03-09 21:44:07 +0900

    👍 use pip module

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM python:3.10.1-buster
 
 COPY main.py /main.py
+COPY requirements.txt /requirements.txt
+
+RUN pip install -r requirements.txt
+# RUN chmod +x src/main.py
 
 ENTRYPOINT ["python", "/main.py"]
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import datetime as dt
 
 
+import requests  # noqa We are just importing this to prove the dependency installed correctly
+
+
 # Set the output value by writing to the outputs in the Environment File, mimicking the behavior defined here:
 #  https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
 def set_github_action_output(output_name, output_value) -> None:
@@ -17,10 +20,14 @@ def set_github_action_output(output_name, output_value) -> None:
 
 
 def main() -> None:
+    print("↓Environment Variables--------------")
     for key, value in os.environ.items():
         print(f"{key}: {value}")
+    print("↑Environment Variables--------------")
 
-    who_to_greet = os.environ.get("INPUT_WHO-TO-GREET")
+    requests.get("https://example.com/")
+
+    who_to_greet = os.environ.get("INPUT_WHO_TO_GREET")
 
     print(f"Hello {who_to_greet}")
 
